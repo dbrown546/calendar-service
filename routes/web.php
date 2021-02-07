8a261ca5319f604edb07217e3858c93adbbb12c8
David Brown <dbrown546@gmail.com>
2021-02-06 23:32:09 -0600

    Update web.php
    dirty routes for demo testing purposes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
+use App\Services\Calendar\CalendarService;
+use Carbon\Carbon;
+
 Route::get('/', function () {
-    return view('welcome');
+    $calendarService = new CalendarService;
+    $startDate = Carbon::now()->addDay();
+    $endDate = Carbon::now()->addWeek();
+
+    $getCalendarBusyTimes = $calendarService->getCalendarBusyTimes($startDate,$endDate);
+    dd($getCalendarBusyTimes);
+});
+
+Route::get('/test', function () {
+    $calendarService = new CalendarService;
+    $startDate = Carbon::now()->addDay();
+    $endDate = Carbon::now()->addWeek();
+
+    $getCalendarFreeTimes = $calendarService->getCalendarFreeTimes($startDate,$endDate, 'America/New_York');
+    dd($getCalendarFreeTimes);
 });
